cf2aa25675d5307bda2a415fc4f564f61fc4b633
Gustavo h. jobstraibizer <gustavojobs_dev@outlook.com>
2021-09-07 09:56:09 -0300

    refactor: Removendo config storybook nao utilizada

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,18 +10,11 @@ module.exports = {
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src/`)
 
-    config.module.rules.push(
-      {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, 'src')
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loaders: ['file-loader'],
-        include: path.resolve(__dirname, 'src')
-      }
-    )
+    config.module.rules.push({
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, 'src')
+    })
 
     return config
   }
